9facde36605319a20b4dca7efe92548888b24a30
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-12-06 12:20:35 +0545

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,90 @@ close.addEventListener('click', () => container.classList.remove('show-nav'))
 // open.addEventListener('click', () => container.classList.add('show-nav'))
 
 // close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'))
+
+// close.addEventListener('click', () => container.classList.remove('show-nav'))
+
